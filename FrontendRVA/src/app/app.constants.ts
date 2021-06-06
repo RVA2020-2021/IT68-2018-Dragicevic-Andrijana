2a98badb7c0338cb85a8c5a1d5bb15ef4e60c940
unknown <adragicevic99@gmail.com>
2021-06-07 00:34:29 +0200

    API endpoints changed

--- FrontendRVA/src/app/app.constants.ts
+++ FrontendRVA/src/app/app.constants.ts
@@ -1,5 +1,11 @@
-export const PROJEKAT_URL = 'http://localhost:8083/projekat';
+/*export const PROJEKAT_URL = 'http://localhost:8083/projekat';
 export const SMER_URL = 'http://localhost:8083/smer';
 export const GRUPA_URL = 'http://localhost:8083/grupa';
 export const STUDENT_URL = 'http://localhost:8083/student';
-export const STUDENTI_U_GRUPI_URL = 'http://localhost:8083/studentiUGrupi'
+export const STUDENTI_U_GRUPI_URL = 'http://localhost:8083/studentiUGrupi'*/
+
+export const PROJEKAT_URL = 'https://backend-rva-2021.herokuapp.com/projekat';
+export const SMER_URL = 'https://backend-rva-2021.herokuapp.com/smer';
+export const GRUPA_URL = 'https://backend-rva-2021.herokuapp.com/grupa';
+export const STUDENT_URL = 'https://backend-rva-2021.herokuapp.com/student';
+export const STUDENTI_U_GRUPI_URL = 'https://backend-rva-2021.herokuapp.com/studentiUGrupi'
